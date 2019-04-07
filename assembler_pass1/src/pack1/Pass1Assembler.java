e824d673a6697c605d359443991f5498388acf6b
Ankit Tiwari <ankit90499@gmail.com>
2019-04-07 15:57:57 +0530

    modified assembler pass1

--- assembler_pass1/src/pack1/Pass1Assembler.java
+++ assembler_pass1/src/pack1/Pass1Assembler.java
@@ -12,8 +12,8 @@ import java.util.Set;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
 class Pass1AssemblerImplementation{
-	// String ST[][]; better to create st hashmap as in generate_ic code we have to search st
-	HashMap<String, String[]> ST;
+	String ST[][]; //better to create st hashmap as in generate_ic code we have to search st
+	
 	String LT[][];
 	int ltindex;
 	int stindex;
@@ -45,7 +45,7 @@ class Pass1AssemblerImplementation{
 			br1 = new ArrayList<String[]>();
 			bwoutput = new BufferedWriter(fileoutput);
 			temp = new MOT_POT();
-			ST = new HashMap<String, String[]>(); // symbol name symbol address symbol length
+			ST = new String[20][4]; // symbol name symbol address symbol length
 			//PT = new Integer[10];
 			LT = new String[10][3]; // literal no literal address
 			ltindex = 1;
@@ -64,13 +64,13 @@ class Pass1AssemblerImplementation{
 		temp.create_POT();
 		try{
 			while( (st = br.readLine() ) != null){
-				starray = st.trim().split("\\s+"); 
+				starray = st.replace(',', ' ').trim().split("\\s+"); 
 				br1.add(starray); // ithink this is used somewhere else
 				i =0;
 				potvalue = temp.find_POT(starray[i]);
 				if(potvalue != null){
 					process_pseudo_op(i);
-					System.out.println(lc);
+//					System.out.println(lc);
 					continue;
 				}
 				motvalue = temp.find_MOT(starray[i]);
@@ -78,17 +78,17 @@ class Pass1AssemblerImplementation{
 					//process literal stored if any 
 					if(starray[i].equals("STOP")){
 						lc +=  Integer.parseInt(motvalue[1]);
-						System.out.println(lc);
+						//System.out.println(lc);
 						continue;
 					}
-					store_literal(starray[i+1], 1); //1 implies its called from here 0 implies called from process_pseudo_op
+					store_literal(i+1, 1); //1 implies its called from here 0 implies called from process_pseudo_op
 					lc +=  Integer.parseInt(motvalue[1]); //parseint return int valueof return Integer object
-					System.out.println(lc);
+//					System.out.println(lc);
 					continue;
 				}
 				//symbol found
 				insert_ST(starray[i]);
-				System.out.println(lc);
+//				System.out.println(lc);
 			}	
 		}catch(Exception e){
 			e.printStackTrace();
@@ -104,7 +104,7 @@ class Pass1AssemblerImplementation{
 			}
 			break;
 		case "DC":
-			store_literal(starray[i+1], 0);
+			store_literal(i+1, 0);
 			break;
 		case "DS":
 			lc += Integer.parseInt(starray[i+1]);
@@ -121,7 +121,7 @@ class Pass1AssemblerImplementation{
 			for(int key : PoolT.keySet()){
 				temp = PoolT.get(key);
 			}
-			System.out.println("temp"+temp+"ltindesx"+ltindex);
+//			System.out.println("temp"+temp+"ltindesx"+ltindex);
 			while(temp < ltindex){
 				if(LT[temp][2] == null){
 					LT[temp][2] = Integer.toString(lc++);
@@ -130,20 +130,26 @@ class Pass1AssemblerImplementation{
 			}
 		}
 	}
-	void store_literal(String key, int flag) {
+	void store_literal(int j, int flag) {
 		if(flag == 0){
 			// find store is called from process pseudo op
 			LT[ltindex][0] = Integer.toString(ltindex);
-			LT[ltindex][1] = key; // literal value;
-			lc += Integer.parseInt(key.replace("'", ""));
+			LT[ltindex][1] = starray[j]; // literal value;
+			lc += Integer.parseInt(starray[j].replace("'", ""));
+			ltindex++;
 		}else{
 			// find store is called from readfile mot
-			starray1 = key.split("=");
-			if(starray1.length == 1){return ;}
-			LT[ltindex][0] = Integer.toString(ltindex);
-			LT[ltindex][1] = starray1[1];
+			for(int k = j ;k < starray.length ;k++){
+				starray1 = starray[k].split("=");
+				if(starray1.length == 1){continue;}
+				LT[ltindex][0] = Integer.toString(ltindex);
+				LT[ltindex][1] = starray1[1];
+				ltindex++;
+				break;
+			}
+			
 		}
-		ltindex++;
+		
 	}
 	void change_LT(){
 		for(int j=PoolT.get(PoolT_index-1) ;j < ltindex; j++){
@@ -153,7 +159,12 @@ class Pass1AssemblerImplementation{
 	}
 	void insert_ST(String key) {
 		// TODO Auto-generated method stub
-		ST.put(key, new String[]{Integer.toString(lc), "1"});//key value(addr, length)
+		//ST.put(key, new String[]{Integer.toString(lc), "1"});//key value(addr, length)
+		ST[stindex][0] = Integer.toString(stindex);
+		ST[stindex][1] = key;
+		ST[stindex][2] = Integer.toString(lc);
+		ST[stindex][3] = "1";
+		stindex++;
 		if(i+1 < starray.length){
 			potvalue = temp.find_POT(starray[i+1]);
 			if(potvalue != null){
@@ -163,45 +174,103 @@ class Pass1AssemblerImplementation{
 				motvalue = temp.find_MOT(starray[i+1]);
 				if(motvalue != null){
 					//process literal stored if any 
-					store_literal(starray[i+1], 1); //1 implies its called from here 0 implies called from process_pseudo_o
+					store_literal(i+1, 1); //1 implies its called from here 0 implies called from process_pseudo_o
 					lc +=  Integer.parseInt(motvalue[1]); //parseint return int valueof return Integer object
 				}
 			}
 		}
 	}
-	void display(){
-		System.out.println("\nSYMBOL TABLE");
-		System.out.println("s_name s_addr s_length");
-		Set keys = ST.keySet();   // It will return you all the keys in Map in the form of the Set
-		for (java.util.Iterator i = keys.iterator(); i.hasNext();) 
-		{
-
-		      String key = (String) i.next();
-		      System.out.print(key+"\t");
-		      String[] values = ST.get(key); // Here is an Individual Record in your HashMap
-		      for(int j=0 ; j < values.length; j++){
-		    	  System.out.print(values[j]+ "\t");
-		      }
-		      System.out.println();
-		}
-		System.out.println("\nLITERAL TABLE");
-		System.out.println("lt_no\tliteral\tlt_addr");
-		for(int i=1 ;i < ltindex; i++){
-			System.out.println(LT[i][0]+"\t"+LT[i][1]+"\t"+LT[i][2]);
+	void display_writetofile(){
+		try{
+			System.out.println("\nSYMBOL TABLE");
+			bwoutput.write("\nSYMBOL TABLE\n");
+			System.out.println("s_no s_name s_addr s_length");
+			bwoutput.write("s_no s_name s_addr s_length\n");
+			for(int i=1 ;i < stindex ;i++){
+				System.out.println(ST[i][0]+"\t"+ST[i][1]+"\t"+ST[i][2]+"\t"+ST[i][3]);
+				bwoutput.write(ST[i][0]+"\t"+ST[i][1]+"\t"+ST[i][2]+"\t"+ST[i][3]+"\n");
+			}
+			System.out.println("\nLITERAL TABLE");
+			bwoutput.write("\nLITERAL TABLE\n");
+			System.out.println("lt_no\tliteral\tlt_addr");
+			bwoutput.write("lt_no\tliteral\tlt_addr\n");
+			for(int i=1 ;i < ltindex; i++){
+				System.out.println(LT[i][0]+"\t"+LT[i][1]+"\t"+LT[i][2]);
+				bwoutput.write(LT[i][0]+"\t"+LT[i][1]+"\t"+LT[i][2]+"\n");
+			}
+			
+			System.out.println("\nLITERAL POOL TABLE");
+			bwoutput.write("\nLITERAL POOL TABLE\n");
+			System.out.println("Index\tLiteralTableIndex");
+			bwoutput.write("Index\tLiteralTableIndex\n");
+			for(int key : PoolT.keySet()){
+				System.out.println(key+"\t"+PoolT.get(key));
+				bwoutput.write(key+"\t"+PoolT.get(key)+"\n");
+			}
+			//TO DEBUG VIEW ARRAYLIST br1 contents
+//			for(String[] temp: br1){
+//				int s = temp.length;
+//				for(int i=0 ;i < s ;i++){
+//					System.out.print(temp[i]+"\t");
+//				}
+//				System.out.println();
+//			});
+			System.out.println("s_no s_name s_addr s_length");
+			for(int i=1 ;i < stindex ;i++){
+				System.out.println(ST[i][0]+"\t"+ST[i][1]+"\t"+ST[i][2]+"\t"+ST[i][3]);
+			}
+			System.out.println("\nLITERAL TABLE");
+			System.out.println("lt_no\tliteral\tlt_addr");
+			for(int i=1 ;i < ltindex; i++){
+				System.out.println(LT[i][0]+"\t"+LT[i][1]+"\t"+LT[i][2]);
+			}
+			
+			System.out.println("\nLITERAL POOL TABLE");
+			System.out.println("Index\tLiteralTableIndex");
+			for(int key : PoolT.keySet()){
+				System.out.println(key+"\t"+PoolT.get(key));
+			}
+			//TO DEBUG VIEW ARRAYLIST br1 contents
+//			for(String[] temp: br1){
+//				int s = temp.length;
+//				for(int i=0 ;i < s ;i++){
+//					System.out.print(temp[i]+"\t");
+//				}
+//				System.out.println();
+//			}
+			for(int i=1 ;i < stindex ;i++){
+				System.out.println(ST[i][0]+"\t"+ST[i][1]+"\t"+ST[i][2]+"\t"+ST[i][3]);
+			}
+			System.out.println("\nLITERAL TABLE");
+			System.out.println("lt_no\tliteral\tlt_addr");
+			for(int i=1 ;i < ltindex; i++){
+				System.out.println(LT[i][0]+"\t"+LT[i][1]+"\t"+LT[i][2]);
+			}
+			
+			System.out.println("\nLITERAL POOL TABLE");
+			System.out.println("Index\tLiteralTableIndex");
+			for(int key : PoolT.keySet()){
+				System.out.println(key+"\t"+PoolT.get(key));
+			}
+			//TO DEBUG VIEW ARRAYLIST br1 contents
+//			for(String[] temp: br1){
+//				int s = temp.length;
+//				for(int i=0 ;i < s ;i++){
+//					System.out.print(temp[i]+"\t");
+//				}
+//				System.out.println();
+//			}
+		}catch(Exception e){
+			e.printStackTrace();
 		}
 		
-		System.out.println("\nLITERAL POOL TABLE");
-		System.out.println("Index\tLiteralTableIndex");
-		for(int key : PoolT.keySet()){
-			System.out.println(key+"\t"+PoolT.get(key));
-		}
 	}
 	void generate_icfile(){
 		try{
 			int i;
 			for(int j=0 ;j < br1.size(); j++){
 				if(j != 0){
-					bwoutput.write("\n");
+					bwoutput.write("\n"); // to
 				}
 				String[] line =  br1.get(j);
 				int len = line.length;
@@ -211,23 +280,55 @@ class Pass1AssemblerImplementation{
 						/*if(i==0){
 							bwoutput.write("\t");
 						}*/
-						if(potvalue.equals("END"))
-						bwoutput.write("AD,"+potvalue+"\t");
+						//if(potvalue.equals("END"))
+						bwoutput.write("(AD,"+potvalue+")\t");
 						continue;
 					}
 					motvalue = temp.find_MOT(line[i]);
 					if(motvalue != null){
-						if(i == 0){
-							bwoutput.write("\t");
-						}
-						bwoutput.write("IS,"+motvalue[0]+"\t");
+//						if(i == 0){
+//							bwoutput.write("\t");
+//						}
+						bwoutput.write("(IS,"+motvalue[0]+")\t");
 						continue;
 					}
-					if(ST.containsKey(line[i])){
-						bwoutput.write(ST.get(line[i])[0]+"\t");
+					if(i == 0) continue; // implies its a symbol defintion whichis skipped
+					
+					// three cases the operand can be symbol, constant, literal, register
+					
+					//if its a literal
+					char c = line[i].charAt(0);
+					if(c == '=' || c == '\''){
+						if(c == '=')line[i] = line[i].split("=")[1];
+//						System.out.println(line[i]);
+						bwoutput.write("(L,"+findliteral(line[i])+")\t");
 						continue;
 					}
-					break;		
+					//if its a symbol
+					String stindex1 = findst(line[i]);
+					if( stindex1 != null){
+						
+						bwoutput.write("(S,"+stindex1+")\t");
+						continue;
+					}
+					// if its a register
+					if(line[i].equals("AREG")){
+						bwoutput.write("(1)");
+						continue;
+					}
+					else if(line[i].equals("BREG")){
+						bwoutput.write("(2)");
+						continue;
+					}
+					else if(line[i].equals("CREG")){
+						bwoutput.write("(3)");
+						continue;
+					}
+					// if its a constant
+					bwoutput.write("(C,"+line[i]+")\t");
+											
+//					
+//					break;		
 				}
 				if(i == len){
 					continue;
@@ -242,6 +343,25 @@ class Pass1AssemblerImplementation{
 		}
 		
 	}
+	String findliteral(String line_i){
+		// get the index where the literal is stored in literal table
+		for(int i=1 ;i < ltindex ;i++){
+			if(LT[i][1].equals(line_i) ){
+				return Integer.toString(i);
+			}
+		}
+		return null;
+	}
+	String findst(String line_i){
+		// get the index where the symbol is stored
+		for(int i=1 ; i< stindex ;i++){
+			if(ST[i][1].equals(line_i)){
+				return Integer.toString(i);
+			}
+		}
+		return null;
+	}
+	
 	void close(){
 		try{
 			bwoutput.close();
@@ -257,8 +377,10 @@ public class Pass1Assembler {
 		Pass1AssemblerImplementation o1 = new Pass1AssemblerImplementation();
 		o1.readfile();
 		o1.generate_icfile();
-		//o1.write_file();
-		o1.display();
+		o1.display_writetofile();//writes symbol table literal table to the file which is to be used
+		// for pass 2
+		 
+		
 		o1.close();
 	}
 }
